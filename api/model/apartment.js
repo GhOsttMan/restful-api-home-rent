8c29f24a36affe3efe0849c889229fac728575de
ghosttman <saem.cse3.bu@gmail.com>
2020-04-06 23:56:23 +0600

    added new function

--- api/model/apartment.js
+++ api/model/apartment.js
@@ -2,8 +2,29 @@ const mongoose = require("mongoose");
 
 const apartmentSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
+  apartmentId: String,
   name: String,
-  price: Number
+  description: String,
+  price: Number,
+  floor: Number,
+
+  address: {
+    road: String,
+    city: String,
+    country: String
+  },
+  roomDetail: {
+    bedroom: Number,
+    dining: Number,
+    bathroom: Number,
+    kitchen: Boolean,
+    balcony: Number,
+    drawingroom: Number
+  },
+  mapCordination: {
+    lat: Number,
+    long: Number
+  }
 });
 
 module.exports = mongoose.model("Apartment", apartmentSchema);
--- api/routes/apartments.js
+++ api/routes/apartments.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 
 const Apartment = require("../model/apartment");
 
+// GET REQUEST
+
 route.get("/", (req, res, next) => {
   Apartment.find()
     .exec()
@@ -14,32 +16,109 @@ route.get("/", (req, res, next) => {
     .catch(err => {
       console.log(err);
       res.status(500).json({
-        message: "Failed"
+        message: "Failed to get all the available apartment"
       });
     });
 });
+
+route.get("/city/:cityName", (req, res, next) => {
+  var name = req.params.cityName;
+  Apartment.find({
+    "address.city": name
+  })
+    .exec()
+    .then(doc => {
+      console.log("city");
+      res.status(200).json(doc);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Failed to get all the available apartment for this city"
+      });
+    });
+});
+
+route.get("/country/:countryName", (req, res, next) => {
+  var name = req.params.countryName;
+  Apartment.find({
+    "address.country": name
+  })
+    .exec()
+    .then(doc => {
+      console.log("country");
+      res.status(200).json(doc);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Failed to get all the available apartment for this country"
+      });
+    });
+});
+
+// POST REQUEST
 
 route.post("/", (req, res, next) => {
   const apartment = new Apartment({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    price: req.body.price
+    price: req.body.price,
+    floor: req.body.floor,
+    description: req.body.description,
+    address: {
+      road: req.body.road,
+      city: req.body.city,
+      country: req.body.country
+    },
+    roomDetail: {
+      bedroom: req.body.bedroom,
+      dining: req.body.dining,
+      bathroom: req.body.bathroom,
+      kitchen: req.body.kitchen,
+      balcony: req.body.balcony,
+      drawingroom: req.body.drawing
+    },
+    mapCordination: {
+      lat: req.body.lat,
+      long: req.body.long
+    }
   });
+  apartment.apartmentId = "" + apartment._id;
   apartment
     .save()
     .then(doc => {
       console.log(doc);
       res.status(200).json({
-        message: "created new entry of apartment",
+        message: "created new entry of an apartment",
         newly_added: apartment
       });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json({
-        message: "Failed to insert"
+        message: "Failed to create new entry"
       });
     });
 });
 
+// DELETE
+
+route.delete("/:apartmentID", (req, res, next) => {
+  const id = req.params.apartmentID;
+  // console.log(apartmentId);
+  Apartment.remove({ apartmentId: id })
+    .exec()
+    .then(doc => {
+      res.status(200).json({
+        msg: "successfully deleted",
+        doc: doc
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        msg: "fail to delete"
+      });
+    });
+});
 module.exports = route;
